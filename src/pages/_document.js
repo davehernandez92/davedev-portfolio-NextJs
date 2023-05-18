98e6c49519328b3c27c81a7fe17257a8db2a06ac
davehernandez92 <davehgal@gmail.com>
2023-05-18 14:35:09 -0300

    meta description

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className='scroll-smooth'>
       <Head>
-        <meta name="description" content="Si estás buscando las mejores opciones de turismo en Acapulco, México, nuestra página web es la guía perfecta para ti. Descubre una amplia variedad de restaurantes y hoteles de alta calidad para que disfrutes de una experiencia única y memorable. Además, encontrarás toda la información que necesitas para explorar los sitios turísticos más destacados de Acapulco. ¡Haz tu reserva ahora y vive una aventura inolvidable en uno de los destinos más populares de México!" />
+        <meta name="description" content="David Hernandez, Front-end developer, Based in Halifax Nova Scotia, Canada " />
         <link rel='stylesheet' href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'}/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
