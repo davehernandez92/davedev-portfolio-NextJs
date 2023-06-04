c3524ed19656c26d0072733e7e64e5e889bc863f
davehernandez92 <davehgal@gmail.com>
2023-06-03 21:04:49 -0300

    initial page loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+import Loader from "./components/loader";
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {isLoading ? (
+       <Loader/>
+        
+        
+      ) : (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 }
+
+export default MyApp;
---
+++ src/pages/components/loader.js
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion"
+function Loader() {
+  return (
+    <div className='loader'>
+        <motion.h1 className='loader__title'
+        initial={{opacity:0, y:30}}
+        animate={{opacity:1, y:0}}
+        transition={{duration: .4}}
+        >Dave</motion.h1>
+        <motion.h1 className='loader__title2'
+        initial={{opacity:0, y:30}}
+        animate={{opacity:1, y:0}}
+        transition={{duration: .8}}
+        
+        >Dev.</motion.h1>
+    
+    </div>
+  )
+}
+
+export default Loader
